457cb6256d9525fe9cf47a95f44e92ae60806105
minhaj-boop <imminhajj@gmail.com>
2022-05-10 20:51:47 +0600

    initial navbar and header done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 
 import Home from './pages/Home/Home.jsx';
+import Hotel from "./pages/Hotel/Hotel.jsx";
 import List from "./pages/List/List.jsx";
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/hotel" element={<List />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
     </Router>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
